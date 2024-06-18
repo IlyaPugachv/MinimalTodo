10cc1629da0b2bcc6aa92e2a56eb1790f7b90411
Ilya Pugachev <ilyapugachv@gmail.com>
2024-06-18 20:13:35 +0300

    Project created

---
+++ MinimalTodo/MinimalTodo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MinimalTodo
+//
+//  Created by Илья Пугачёв on 18.06.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
