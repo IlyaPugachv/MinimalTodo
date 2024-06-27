acba944e380ac5f43326b362312a9fdc9f7bbcb9
Ilya Pugachev <ilyapugachv@gmail.com>
2024-06-27 14:43:59 +0300

    Added custom UIColor Extensions

---
+++ MinimalTodo/MinimalTodo/AppConstans/Color/UIColor+Extensions.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+extension UIColor {
+    struct Colors {
+        static let yellow = #colorLiteral(red: 0.9764705896, green: 0.850980401, blue: 0.5490196347, alpha: 1)
+        static let green = #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1)
+        static let violet = #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)
+        static let red = #colorLiteral(red: 1, green: 0.1491314173, blue: 0, alpha: 1)
+    }
+}
