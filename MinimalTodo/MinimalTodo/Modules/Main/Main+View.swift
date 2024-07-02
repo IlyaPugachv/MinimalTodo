ef3a11cf4b49808b61ddc3663e17d1521a685ae9
Ilya Pugachev <ilyapugachv@gmail.com>
2024-07-02 13:17:21 +0300

    fix

--- MinimalTodo/MinimalTodo/Modules/Main/Main+View.swift
+++ MinimalTodo/MinimalTodo/Modules/Main/Main+View.swift
@@ -8,6 +8,9 @@ extension Main {
         var presenter: Presenter!
         private lazy var safeArea = self.view.safeAreaLayoutGuide
         private var todoLists: [TodoList] = []
+        private var searchResults: [TodoList] = []
+        private var searchView: SearchView!
+        private let todoListCellIdentifier = "TodoListCell"
         
         // MARK: - Subviews -
         
@@ -68,6 +71,27 @@ extension Main {
             view.addView(createTodoLabel)
             view.addView(noPinnedLabel)
             view.addView(newListButton)
+            setupSearchView()
+        }
+        
+        private func setupSearchView() {
+            searchView = SearchView()
+            searchView.isHidden = true
+            searchView.searchBar.delegate = self
+            searchView.tableView.dataSource = self
+            searchView.tableView.delegate = self
+            
+            // Регистрация класса ячейки
+            searchView.tableView.register(UITableViewCell.self, forCellReuseIdentifier: todoListCellIdentifier)
+            
+            view.addView(searchView)
+            
+            NSLayoutConstraint.activate([
+                searchView.topAnchor.constraint(equalTo: safeArea.topAnchor),
+                searchView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
+                searchView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor),
+                searchView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor),
+            ])
         }
         
         private func configureSubviews() {
@@ -150,6 +174,53 @@ extension Main {
                 guard let self = self else { return }
                 self.presenter.goToNewListScreen()
             }), for: .touchUpInside)
+            
+            let searchTapGesture = UITapGestureRecognizer(target: self, action: #selector(handleSearchTap))
+            searchImageView.addGestureRecognizer(searchTapGesture)
+            searchImageView.isUserInteractionEnabled = true
+        }
+        
+        @objc private func handleSearchTap() {
+            searchView.isHidden = !searchView.isHidden
+        }
+        
+        private func loadTodoListsFromUserDefaults() {
+            if let savedData = UserDefaults.standard.data(forKey: "TodoLists") {
+                let decoder = JSONDecoder()
+                if let loadedLists = try? decoder.decode([TodoList].self, from: savedData) {
+                    todoLists = loadedLists
+                }
+            }
+        }
+        
+        private func saveTodoListsToUserDefaults() {
+            let encoder = JSONEncoder()
+            if let encoded = try? encoder.encode(todoLists) {
+                UserDefaults.standard.set(encoded, forKey: "TodoLists")
+            }
+        }
+        
+        private func filterTodoLists() -> [TodoList] {
+            let selectedSegmentIndex = segmentedControl.selectedSegmentIndex
+            switch selectedSegmentIndex {
+            case 0:
+                return todoLists.filter { !$0.isPinned } // All Lists excluding pinned
+            case 1:
+                return todoLists.filter { $0.isPinned } // Pinned Lists
+            default:
+                return todoLists.filter { !$0.isPinned } // Default to All Lists excluding pinned
+            }
+        }
+        
+        public func addTodoList(_ todoList: TodoList) {
+            var newTodoList = todoList
+            newTodoList.assignRandomColor()
+            todoLists.append(newTodoList)
+            saveTodoListsToUserDefaults()
+            
+            //            // Set the segmented control to "All Lists"
+            //            segmentedControl.selectedSegmentIndex = 0
+            updateUI()
         }
         
         private func updateUI() {
@@ -195,45 +266,6 @@ extension Main {
                 }
             }
         }
-        
-        private func filterTodoLists() -> [TodoList] {
-            let selectedSegmentIndex = segmentedControl.selectedSegmentIndex
-            switch selectedSegmentIndex {
-            case 0:
-                return todoLists.filter { !$0.isPinned } // All Lists excluding pinned
-            case 1:
-                return todoLists.filter { $0.isPinned } // Pinned Lists
-            default:
-                return todoLists.filter { !$0.isPinned } // Default to All Lists excluding pinned
-            }
-        }
-        
-        private func loadTodoListsFromUserDefaults() {
-            if let savedData = UserDefaults.standard.data(forKey: "TodoLists") {
-                let decoder = JSONDecoder()
-                if let loadedLists = try? decoder.decode([TodoList].self, from: savedData) {
-                    todoLists = loadedLists
-                }
-            }
-        }
-        
-        public func addTodoList(_ todoList: TodoList) {
-            var newTodoList = todoList
-            newTodoList.assignRandomColor()
-            todoLists.append(newTodoList)
-            saveTodoListsToUserDefaults()
-            
-            //            // Set the segmented control to "All Lists"
-            //            segmentedControl.selectedSegmentIndex = 0
-            updateUI()
-        }
-        
-        private func saveTodoListsToUserDefaults() {
-            let encoder = JSONEncoder()
-            if let encoded = try? encoder.encode(todoLists) {
-                UserDefaults.standard.set(encoded, forKey: "TodoLists")
-            }
-        }
     }
 }
 
@@ -255,3 +287,305 @@ extension Main.View: CustomSegmentedControlDelegate {
         updateUI()
     }
 }
+
+extension Main.View: UISearchBarDelegate, UITableViewDataSource, UITableViewDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        filterTodoLists(for: searchText)
+    }
+    
+    private func filterTodoLists(for query: String) {
+        searchResults = todoLists.filter { $0.title.lowercased().contains(query.lowercased()) }
+        searchView.tableView.reloadData()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return searchResults.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: todoListCellIdentifier, for: indexPath)
+        
+        // Remove existing subviews to avoid duplication
+        cell.contentView.subviews.forEach { $0.removeFromSuperview() }
+        
+        let todoList = searchResults[indexPath.row]
+        let todoListView = TodoListView(todoList: todoList)
+        todoListView.delegate = self
+        todoListView.translatesAutoresizingMaskIntoConstraints = false
+        
+        cell.contentView.addSubview(todoListView)
+        
+        NSLayoutConstraint.activate([
+            todoListView.leadingAnchor.constraint(equalTo: cell.contentView.leadingAnchor),
+            todoListView.trailingAnchor.constraint(equalTo: cell.contentView.trailingAnchor),
+            todoListView.topAnchor.constraint(equalTo: cell.contentView.topAnchor),
+            todoListView.bottomAnchor.constraint(equalTo: cell.contentView.bottomAnchor),
+            todoListView.heightAnchor.constraint(equalToConstant: 80),
+        ])
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        // Handle todo list selection
+    }
+}
+//
+//import UIKit
+//
+//extension Main {
+//    class View: UIViewController {
+//        
+//        // MARK: - Properties -
+//        
+//        var presenter: Presenter!
+//        private lazy var safeArea = self.view.safeAreaLayoutGuide
+//        private var todoLists: [TodoList] = []
+//        
+//        // MARK: - Subviews -
+//        
+//        private let appMiniIconImageView: UIImageView = .init(image: .applicationMiniIcon)
+//        private let nameAppLabel: UILabel = .init()
+//        private let searchImageView: UIImageView = .init()
+//        private let segmentedControl: CustomSegmentedControl = .init()
+//        private let appImage: UIImageView = .init(image: .imageApp)
+//        private let appImageOne: UIImageView = .init(image: .imageAppOne)
+//        private let createTodoLabel: UILabel = .init()
+//        private let noPinnedLabel: UILabel = .init()
+//        private let newListButton: UIButton = .init()
+//        
+//        // MARK: - Initializers -
+//        
+//        public init(with presenter: Presenter) {
+//            self.presenter = presenter
+//            super.init(nibName: nil, bundle: nil)
+//            presenter.view = self
+//        }
+//        
+//        required init?(coder: NSCoder) {
+//            fatalError("init(coder:) has not been implemented")
+//        }
+//        
+//        deinit { }
+//        
+//        // MARK: - Lifecycle -
+//        
+//        public override func viewDidLoad() {
+//            super.viewDidLoad()
+//            setup()
+//            loadTodoListsFromUserDefaults()
+//        }
+//        
+//        public override func viewWillAppear(_ animated: Bool) {
+//            super.viewWillAppear(animated)
+//            updateUI()
+//        }
+//        
+//        // MARK: - Methods -
+//        
+//        private func setup() {
+//            buildHierarchy()
+//            configureSubviews()
+//            layoutSubviews()
+//            setupActions()
+//        }
+//        
+//        private func buildHierarchy() {
+//            view.backgroundColor = .white
+//            view.addView(appMiniIconImageView)
+//            view.addView(nameAppLabel)
+//            view.addView(searchImageView)
+//            view.addView(segmentedControl)
+//            view.addView(appImage)
+//            view.addView(appImageOne)
+//            view.addView(createTodoLabel)
+//            view.addView(noPinnedLabel)
+//            view.addView(newListButton)
+//        }
+//        
+//        private func configureSubviews() {
+//            navigationItem.hidesBackButton = true
+//            
+//            segmentedControl.delegate = self
+//            
+//            appMiniIconImageView.contentMode = .scaleAspectFit
+//            
+//            nameAppLabel.configureLabel(
+//                text: "MinimalTodo",
+//                font: .interSemibold(of: 20),
+//                color: .black
+//            )
+//            
+//            searchImageView.image = UIImage(systemName: "magnifyingglass")
+//            searchImageView.tintColor = .black
+//            
+//            createTodoLabel.configureLabel(
+//                text: .Localization.createYourFirstTodoList,
+//                font: .interSemibold(of: 20),
+//                color: .black
+//            )
+//            
+//            noPinnedLabel.configureLabel(
+//                text: .Localization.ooopsNoPinnedListYet,
+//                font: .interSemibold(of: 20),
+//                color: .black
+//            )
+//            
+//            newListButton.backgroundColor = .black
+//            newListButton.layer.cornerRadius = 32.5
+//            
+//            let plusImage = UIImage(systemName: "plus")?.withRenderingMode(.alwaysTemplate)
+//            newListButton.setImage(plusImage, for: .normal)
+//            newListButton.tintColor = .white
+//        }
+//        
+//        private func layoutSubviews() {
+//            NSLayoutConstraint.activate([
+//                appMiniIconImageView.topAnchor.constraint(equalTo: safeArea.topAnchor),
+//                appMiniIconImageView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 10),
+//                appMiniIconImageView.widthAnchor.constraint(equalToConstant: 31),
+//                appMiniIconImageView.heightAnchor.constraint(equalToConstant: 26),
+//                
+//                nameAppLabel.topAnchor.constraint(equalTo: appMiniIconImageView.topAnchor),
+//                nameAppLabel.leadingAnchor.constraint(equalTo: appMiniIconImageView.trailingAnchor, constant: 10),
+//                
+//                searchImageView.topAnchor.constraint(equalTo: appMiniIconImageView.topAnchor),
+//                searchImageView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -10),
+//                searchImageView.widthAnchor.constraint(equalToConstant: 24),
+//                searchImageView.heightAnchor.constraint(equalToConstant: 24),
+//                
+//                segmentedControl.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: 65),
+//                segmentedControl.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 20),
+//                segmentedControl.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -20),
+//                segmentedControl.heightAnchor.constraint(equalToConstant: 47),
+//                
+//                appImage.topAnchor.constraint(equalTo: segmentedControl.bottomAnchor, constant: 50),
+//                appImage.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+//                
+//                appImageOne.topAnchor.constraint(equalTo: segmentedControl.bottomAnchor, constant: 50),
+//                appImageOne.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+//                
+//                createTodoLabel.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor, constant: -160),
+//                createTodoLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+//                
+//                noPinnedLabel.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor, constant: -160),
+//                noPinnedLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+//                
+//                newListButton.topAnchor.constraint(equalTo: createTodoLabel.bottomAnchor, constant: 25),
+//                newListButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+//                newListButton.widthAnchor.constraint(equalToConstant: 65),
+//                newListButton.heightAnchor.constraint(equalToConstant: 65),
+//            ])
+//        }
+//        
+//        private func setupActions() {
+//            newListButton.addAction(UIAction(handler: { [weak self] _ in
+//                guard let self = self else { return }
+//                self.presenter.goToNewListScreen()
+//            }), for: .touchUpInside)
+//        }
+//        
+//        private func updateUI() {
+//            view.subviews.forEach { subview in
+//                if subview is TodoListView {
+//                    subview.removeFromSuperview()
+//                }
+//            }
+//            
+//            let filteredTodoLists = filterTodoLists()
+//            
+//            if filteredTodoLists.isEmpty {
+//                if segmentedControl.selectedSegmentIndex == 0 {
+//                    appImage.isHidden = false
+//                    appImageOne.isHidden = true
+//                    createTodoLabel.isHidden = false
+//                    noPinnedLabel.isHidden = true
+//                } else if segmentedControl.selectedSegmentIndex == 1 {
+//                    appImage.isHidden = true
+//                    appImageOne.isHidden = false
+//                    createTodoLabel.isHidden = true
+//                    noPinnedLabel.isHidden = false
+//                }
+//            } else {
+//                appImage.isHidden = true
+//                appImageOne.isHidden = true
+//                createTodoLabel.isHidden = true
+//                noPinnedLabel.isHidden = true
+//                
+//                var previousView: UIView? = segmentedControl
+//                for todoList in filteredTodoLists {
+//                    let todoListView = TodoListView(todoList: todoList)
+//                    todoListView.delegate = self
+//                    view.addView(todoListView)
+//                    
+//                    NSLayoutConstraint.activate([
+//                        todoListView.topAnchor.constraint(equalTo: previousView?.bottomAnchor ?? view.topAnchor, constant: 20),
+//                        todoListView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 20),
+//                        todoListView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -20),
+//                    ])
+//                    
+//                    previousView = todoListView
+//                }
+//            }
+//        }
+//        
+//        private func filterTodoLists() -> [TodoList] {
+//            let selectedSegmentIndex = segmentedControl.selectedSegmentIndex
+//            switch selectedSegmentIndex {
+//            case 0:
+//                return todoLists.filter { !$0.isPinned } // All Lists excluding pinned
+//            case 1:
+//                return todoLists.filter { $0.isPinned } // Pinned Lists
+//            default:
+//                return todoLists.filter { !$0.isPinned } // Default to All Lists excluding pinned
+//            }
+//        }
+//        
+//        private func loadTodoListsFromUserDefaults() {
+//            if let savedData = UserDefaults.standard.data(forKey: "TodoLists") {
+//                let decoder = JSONDecoder()
+//                if let loadedLists = try? decoder.decode([TodoList].self, from: savedData) {
+//                    todoLists = loadedLists
+//                }
+//            }
+//        }
+//        
+//        public func addTodoList(_ todoList: TodoList) {
+//            var newTodoList = todoList
+//            newTodoList.assignRandomColor()
+//            todoLists.append(newTodoList)
+//            saveTodoListsToUserDefaults()
+//            
+//            //            // Set the segmented control to "All Lists"
+//            //            segmentedControl.selectedSegmentIndex = 0
+//            updateUI()
+//        }
+//        
+//        private func saveTodoListsToUserDefaults() {
+//            let encoder = JSONEncoder()
+//            if let encoded = try? encoder.encode(todoLists) {
+//                UserDefaults.standard.set(encoded, forKey: "TodoLists")
+//            }
+//        }
+//    }
+//}
+//
+//extension Main.View: MainView, TodoListViewDelegate {
+//    func todoListViewDidSwipeToDelete(_ todoListView: TodoListView) {
+//        guard let index = todoLists.firstIndex(where: { $0.id == todoListView.todoList.id }) else { return }
+//        
+//        todoListView.animateDeletion { [weak self] in
+//            guard let self = self else { return }
+//            self.todoLists.remove(at: index)
+//            self.saveTodoListsToUserDefaults()
+//            self.updateUI()
+//        }
+//    }
+//}
+//
+//extension Main.View: CustomSegmentedControlDelegate {
+//    func segmentedControlChanged(to index: Int) {
+//        updateUI()
+//    }
+//}
